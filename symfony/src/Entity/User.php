22f8526384b622e7f7e886ca4f42b677c1baea7e
Konstantin Abramovskiy <kostyaab@gmail.com>
2021-02-28 21:35:46 +0700

    * php to 8.0 && composer update * doctrine/orm * symfony/maker-bundle * wip: user entity/controller * disable xdebug config

---
+++ symfony/src/Entity/User.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Table(name="`user`")
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
+    private string $tel;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTel(): ?string
+    {
+        return $this->tel;
+    }
+
+    public function setTel(string $tel): self
+    {
+        $this->tel = $tel;
+
+        return $this;
+    }
+}
